e62ee24d44773441e9fa209750a08ceeae44fd6d
Nickolay Kharchevin <n.kharchevin@verta.media>
2018-02-01 20:24:04 +0200

    Initial

---
+++ index.js
@@ -0,0 +1,7 @@
+/*
+  MIT License http://www.opensource.org/licenses/mit-license.php
+  Author Nickolay "mbIkola" Kharchevin <kharchevyn@gmail.com>
+*/
+module.exports = function(source) {
+    return module.exports = "hex string will be there ";
+}
