e1961e080ee6de52e4b1e43ea96947aa5e63e037
Nickolay Kharchevin <n.kharchevin@verta.media>
2018-02-01 21:42:27 +0200

    Да, я трогал себя руками когда писал этот код

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 /*
  *   MIT License http://www.opensource.org/licenses/mit-license.php
  *     Author Nickolay "mbIkola" Kharchevin <kharchevyn@gmail.com>
- *     */
+ *
+ */
 module.exports = function(source) {
-        let str = Buffer.from(source).toString('hex');
-            return 'module.exports = "' + str + '"' ;
+        return 'module.exports = "' + source.toString('hex') + '"' ;
 }
+module.exports.raw = true;
+
+
 
 
