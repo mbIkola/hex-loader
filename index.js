80ed22854c93ee9faf93717292a01d5703310b07
Nickolay Kharchevin <n.kharchevin@verta.media>
2018-02-01 20:38:44 +0200

    hex string

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 /*
-  MIT License http://www.opensource.org/licenses/mit-license.php
-  Author Nickolay "mbIkola" Kharchevin <kharchevyn@gmail.com>
-*/
+ *   MIT License http://www.opensource.org/licenses/mit-license.php
+ *     Author Nickolay "mbIkola" Kharchevin <kharchevyn@gmail.com>
+ *     */
 module.exports = function(source) {
-    return module.exports = "hex string will be there ";
+        let str = Buffer.from(source).toString('hex');
+            return 'module.exports = "' + str + '"' ;
 }
+
+
